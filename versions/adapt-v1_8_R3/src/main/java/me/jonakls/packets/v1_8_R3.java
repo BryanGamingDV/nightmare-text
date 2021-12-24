0a46bdd4f9c69730f3eb697f5f1536038817c180
jonakls <asmot54@hotmail.com>
2021-12-24 11:28:20 -0500

    feat: create packets of titles in mc 1.8.8

---
+++ versions/adapt-v1_8_R3/src/main/java/me/jonakls/packets/v1_8_R3.java
@@ -0,0 +1,49 @@
+package me.jonakls.packets;
+
+import me.jonakls.api.PacketModel;
+import me.jonakls.api.components.ActionBarModel;
+import me.jonakls.api.components.TitleModel;
+import net.minecraft.server.v1_8_R3.*;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class v1_8_R3 implements PacketModel {
+
+    @Override
+    public void sendTitle(Player player, TitleModel titleModel) {
+        PacketPlayOutTitle titlePacket = new PacketPlayOutTitle(
+                PacketPlayOutTitle.EnumTitleAction.TITLE,
+                new ChatMessage(titleModel.getTitle()),
+                titleModel.getFadeIn(),
+                titleModel.getStay(),
+                titleModel.getFadeOut()
+        );
+
+        PacketPlayOutTitle subTitlePacket = new PacketPlayOutTitle(
+                PacketPlayOutTitle.EnumTitleAction.SUBTITLE,
+                new ChatMessage(titleModel.getSubTitle()),
+                titleModel.getFadeIn(),
+                titleModel.getStay(),
+                titleModel.getFadeOut()
+        );
+
+        sendPacket(player, titlePacket);
+        sendPacket(player, subTitlePacket);
+
+    }
+
+    @Override
+    public void sendActionBar(Player player, ActionBarModel actionBarModel) {
+        PacketPlayOutChat actionBarPacket = new PacketPlayOutChat(
+                IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + actionBarModel.getActionBar() + "\"}"),
+                (byte) 2
+        );
+
+        sendPacket(player, actionBarPacket);
+    }
+
+    @Override
+    public void sendPacket(Player player, Object packet) {
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket((Packet) packet);
+    }
+}
