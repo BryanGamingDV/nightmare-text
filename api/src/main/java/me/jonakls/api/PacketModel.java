c56c5ead7844ecb61547a7ce02c21ed473df8687
jonakls <asmot54@hotmail.com>
2021-12-24 11:26:07 -0500

    feat: added methods of models

--- api/src/main/java/me/jonakls/api/PacketModel.java
+++ api/src/main/java/me/jonakls/api/PacketModel.java
@@ -1,8 +1,13 @@
 package me.jonakls.api;
 
+import me.jonakls.api.components.ActionBarModel;
+import me.jonakls.api.components.TitleModel;
+import org.bukkit.entity.Player;
+
 public interface PacketModel {
     
-    void createPacket(Object type);
-    void sendPacket();
+    void sendTitle(Player player, TitleModel titleModel);
+    void sendActionBar(Player player, ActionBarModel actionBarModel);
+    void sendPacket(Player player, Object packet);
 
 }
