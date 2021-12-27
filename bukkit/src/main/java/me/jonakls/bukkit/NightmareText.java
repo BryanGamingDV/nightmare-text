ac2e1b2802b5eef717808a913ef4e71c121b9c2c
jonakls <asmot54@hotmail.com>
2021-12-27 01:55:30 -0500

    feat: created general instance for use
    A general instance is created for general lib usage from bukkit.

--- bukkit/src/main/java/me/jonakls/bukkit/NightmareText.java
+++ bukkit/src/main/java/me/jonakls/bukkit/NightmareText.java
@@ -1,6 +1,18 @@
 package me.jonakls.bukkit;
 
+import me.jonakls.api.PacketModel;
+import me.jonakls.bukkit.version.ServerVersion;
+
 public class NightmareText {
 
+    private PacketModel packetModel;
 
+    public void install() {
+        ServerVersion version = new ServerVersion();
+        packetModel = version.getModelVersion();
+    }
+
+    public PacketModel getPacketModel() {
+        return packetModel;
+    }
 }
