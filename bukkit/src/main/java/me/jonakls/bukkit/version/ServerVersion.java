3959038081eec5554dc47740832b1a45e1026c3c
jonakls <asmot54@hotmail.com>
2021-12-27 13:47:25 -0500

    feat: added instances in bukkit subproject

--- bukkit/src/main/java/me/jonakls/bukkit/version/ServerVersion.java
+++ bukkit/src/main/java/me/jonakls/bukkit/version/ServerVersion.java
@@ -1,9 +1,17 @@
 package me.jonakls.bukkit.version;
 
 import me.jonakls.api.PacketModel;
+import me.jonakls.packets.v1_10_R1;
+import me.jonakls.packets.v1_11_R1;
+import me.jonakls.packets.v1_13_R1;
+import me.jonakls.packets.v1_14_R1;
+import me.jonakls.packets.v1_15_R1;
+import me.jonakls.packets.v1_16_R1;
 import me.jonakls.packets.v1_17_R1;
+import me.jonakls.packets.v1_18_R1;
 import me.jonakls.packets.v1_8_R3;
 import me.jonakls.packets.v1_9_R2;
+import me.jonkls.packets.v1_12_R1;
 import org.bukkit.Bukkit;
 
 public class ServerVersion {
@@ -17,7 +25,31 @@ public class ServerVersion {
         if (version.contains("1_9_R2")) {
             return new v1_9_R2();
         }
-        return new v1_17_R1();
+        if (version.contains("1_10_R1")) {
+            return new v1_10_R1();
+        }
+        if (version.contains("1_11_R1")) {
+            return new v1_11_R1();
+        }
+        if (version.contains("1_12_R1")) {
+            return new v1_12_R1();
+        }
+        if (version.contains("1_13_R1")) {
+            return new v1_13_R1();
+        }
+        if (version.contains("1_14_R1")) {
+            return new v1_14_R1();
+        }
+        if (version.contains("1_15_R1")) {
+            return new v1_15_R1();
+        }
+        if (version.contains("1_16_R1")) {
+            return new v1_16_R1();
+        }
+        if (version.contains("1_17_R1")) {
+            return new v1_17_R1();
+        }
+        return new v1_18_R1();
 
     }
 
