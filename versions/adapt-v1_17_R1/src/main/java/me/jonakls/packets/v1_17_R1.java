f309b24b7f317703f908ca4cdfd3c4c3a8cf7055
jonakls <asmot54@hotmail.com>
2021-12-27 01:53:58 -0500

    feat: added implementations of api in modules of sub versions

--- versions/adapt-v1_17_R1/src/main/java/me/jonakls/packets/v1_17_R1.java
+++ versions/adapt-v1_17_R1/src/main/java/me/jonakls/packets/v1_17_R1.java
@@ -1,4 +1,24 @@
 package me.jonakls.packets;
 
-public class v1_17_R1 {
+import me.jonakls.api.PacketModel;
+import me.jonakls.api.components.ActionBarModel;
+import me.jonakls.api.components.TitleModel;
+import org.bukkit.entity.Player;
+
+public class v1_17_R1 implements PacketModel {
+
+    @Override
+    public void sendTitle(Player player, TitleModel titleModel) {
+
+    }
+
+    @Override
+    public void sendActionBar(Player player, ActionBarModel actionBarModel) {
+
+    }
+
+    @Override
+    public void sendPacket(Player player, Object packet) {
+
+    }
 }
