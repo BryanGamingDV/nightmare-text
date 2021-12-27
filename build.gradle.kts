62c0924ea4f3508be4b9c744088029208c5590b5
jonakls <asmot54@hotmail.com>
2021-12-27 13:46:28 -0500

    build: added properties of general project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,12 @@
 plugins {
     java
+    `maven-publish`
     id("com.github.johnrengelman.shadow") version("7.1.1")
 }
+
+group = "me.jonakls"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 subprojects {
     apply(plugin="java")
@@ -10,5 +15,11 @@ subprojects {
     repositories {
         mavenCentral()
         maven("https://repo.codemc.io/repository/nms/")
+    }
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
     }
 }
