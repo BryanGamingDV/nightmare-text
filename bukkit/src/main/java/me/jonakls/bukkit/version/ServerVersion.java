949783203712097e6afaf6cfa6899ad66b9b6d34
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2021-12-27 22:22:21 -0500

    feat: updated code

--- bukkit/src/main/java/me/jonakls/bukkit/version/ServerVersion.java
+++ bukkit/src/main/java/me/jonakls/bukkit/version/ServerVersion.java
@@ -13,40 +13,39 @@ import me.jonakls.packets.v1_9_R2;
 import me.jonakls.packets.v1_12_R1;
 import org.bukkit.Bukkit;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ServerVersion {
 
+    private final Map<String, PacketModel> packetModelMap = new HashMap<>();
+
+    public ServerVersion(){
+        startVersion("1.8", new v1_8_R3())
+                .startVersion("1.9", new v1_9_R2())
+                .startVersion("1.10", new v1_10_R1())
+                .startVersion("1.11", new v1_11_R1())
+                .startVersion("1.12", new v1_12_R1())
+                .startVersion("1.13", new v1_13_R1())
+                .startVersion("1.14", new v1_14_R1())
+                .startVersion("1.15", new v1_15_R1())
+                .startVersion("1.16", new v1_16_R1())
+                .startVersion("1.17", new v1_17_R1());
+
+    }
+
     public PacketModel getModelVersion() {
-        String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
 
-        if (version.contains("1_8_R3")) {
-            return new v1_8_R3();
-        }
-        if (version.contains("1_9_R2")) {
-            return new v1_9_R2();
-        }
-        if (version.contains("1_10_R1")) {
-            return new v1_10_R1();
-        }
-        if (version.contains("1_11_R1")) {
-            return new v1_11_R1();
-        }
-        if (version.contains("1_12_R1")) {
-            return new v1_12_R1();
-        }
-        if (version.contains("1_13_R1")) {
-            return new v1_13_R1();
-        }
-        if (version.contains("1_14_R1")) {
-            return new v1_14_R1();
-        }
-        if (version.contains("1_15_R1")) {
-            return new v1_15_R1();
-        }
-        if (version.contains("1_16_R1")) {
-            return new v1_16_R1();
-        }
-        return new v1_17_R1();
+        String version = Bukkit.getServer().getClass().getName();
+        String versionName = version.split("\\.")[3].substring(0, version.split("\\.")[3].indexOf("_", 3));
 
+        return packetModelMap.get(versionName);
+
+    }
+
+    public ServerVersion startVersion(String version, PacketModel packetModel){
+        packetModelMap.put(version, packetModel);
+        return this;
     }
 
 }
